b82bdae1081a12fbab6404e22b2b243d9fe7e692
EMONGAZI19 <afranemon84@gmail.com>
2025-05-12 01:10:22 +0600

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,49 +1,128 @@
-const movies = {
-  latestMovies: [
-    { title: 'Borbaad', image: 'https://m.media-amazon.com/images/M/MV5BNDY1ZWQyNjMtYTJjMC00ZjM3LTk3MWItMTcwNjg4ODZmODZhXkEyXkFqcGc@._V1_QL75_UX704_.jpg', link: 'https://movieflix9.netlify.app/borbaadhd' },
-    { title: 'Magic Movie', image: 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p26313078_v_v8_aa.jpg', link: 'https://movieflix9.netlify.app/magic' },
-    { title: 'The Bhootni', image: 'https://i.postimg.cc/mDxKxYRF/images-4.jpg', link: 'https://movieflix9.netlify.app/bhootni' },
-    { title: 'Jongli', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQgqAc6VFQWRzapb9n60cSTH7w_VNovCjgufyFY1ll6x-iaWJwB1xnv9Im&s=10', link: 'https://movieflix9.netlify.app/jongli' },
-    { title: 'Jaat (2025)', image: 'https://m.media-amazon.com/images/M/MV5BNDQxYmM2YmUtOGJmYy00YzcyLTkxOTItYmI3ZTZjZmUwZDMxXkEyXkFqcGc@._V1_FMjpg_UY2048_.jpg', link: 'https://movieflix9.netlify.app/jaat' },
-    { title: 'Movie 6', image: 'https://m.media-amazon.com/images/M/MV5BNDY1ZWQyNjMtYTJjMC00ZjM3LTk3MWItMTcwNjg4ODZmODZhXkEyXkFqcGc@._V1_QL75_UX704_.jpg', link: 'https://example.com/movie6' },
-    { title: 'Movie 7', image: 'https://m.media-amazon.com/images/M/MV5BNDY1ZWQyNjMtYTJjMC00ZjM3LTk3MWItMTcwNjg4ODZmODZhXkEyXkFqcGc@._V1_QL75_UX704_.jpg', link: 'https://example.com/movie7' },
-    { title: 'Movie 8', image: 'movie8.jpg', link: 'https://example.com/movie8' },
-    { title: 'Movie 9', image: 'movie9.jpg', link: 'https://example.com/movie9' },
-    { title: 'Movie 10', image: 'movie10.jpg', link: 'https://example.com/movie10' },
-    { title: 'Movie 11', image: 'movie11.jpg', link: 'https://example.com/movie11' },
-    { title: 'Movie 12', image: 'movie12.jpg', link: 'https://example.com/movie12' },
-    { title: 'Movie 13', image: 'movie13.jpg', link: 'https://example.com/movie13' },
-    { title: 'Movie 14', image: 'movie14.jpg', link: 'https://example.com/movie14' },
-    { title: 'Movie 15', image: 'movie15.jpg', link: 'https://example.com/movie15' }
-  ],
-  banglaMovies: [
-    { title: 'Jongli (2025)', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQgqAc6VFQWRzapb9n60cSTH7w_VNovCjgufyFY1ll6x-iaWJwB1xnv9Im&s=10', link: 'https://movieflix9.netlify.app/jongli' },
-    // Add more movies here...
-  ],
-  hindiDubbedMovies: [
-    { title: 'Hindi Movie 1', image: 'hindi1.jpg', link: 'https://example.com/hindi1' },
-    // Add more movies here...
-  ],
-  hollywoodMovies: [
-    { title: 'Hollywood Movie 1', image: 'hollywood1.jpg', link: 'https://example.com/hollywood1' },
-    // Add more movies here...
-  ],
-  banglaDubbedMovies: [
-    { title: 'Bangla Dubbed Movie 1', image: 'bangladubbed1.jpg', link: 'https://example.com/bangladubbed1' },
-    // Add more movies here...
-  ],
-  tvwebseries: [
-    { title: 'Emoji', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyBGj3VcgaQ2J9I3tl68_HC84kNulomfDthLV2FcZKK_RfO_mj0VOiAGw&s=10', link: 'https://movieflix9.netlify.app/emoji' },
-    { title: 'Bhog (2025)', image: 'https://m.media-amazon.com/images/M/MV5BZjllZjRiZGEtNzc2Mi00ZmFmLTllYWEtYzYxMThhMTgxYjM3XkEyXkFqcGc@._V1_QL75_UX808_.jpg', link: 'https://movieflix9.netlify.app/bhog' },
-    { title: 'tvwebseries', image: 'movie9.jpg', link: 'https://example.com/movie9' },
-    { title: 'tvwebseries', image: 'movie10.jpg', link: 'https://example.com/movie10' },
-    { title: 'tvwebseries', image: 'movie11.jpg', link: 'https://example.com/movie11' },
-    { title: 'tvwebseries', image: 'movie12.jpg', link: 'https://example.com/movie12' },
-    { title: 'tvwebseries', image: 'movie13.jpg', link: 'https://example.com/movie13' },
-    { title: 'tvwebseries', image: 'movie14.jpg', link: 'https://example.com/movie14' },
-  ],
-  animeMovies: [
-    { title: 'Anime Movie 1', image: 'anime1.jpg', link: 'https://example.com/anime1' },
-    // Add more movies here...
-  ]
-};
+const movies = [
+  {
+    title: 'Borbaad',
+    image: 'https://m.media-amazon.com/images/M/MV5BNDY1ZWQyNjMtYTJjMC00ZjM3LTk3MWItMTcwNjg4ODZmODZhXkEyXkFqcGc@._V1_QL75_UX704_.jpg',
+    link: 'https://movieflix9.netlify.app/borbaadhd',
+    categories: ['latestMovies', 'banglaMovies']
+  },
+  {
+    title: 'Magic Movie',
+    image: 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p26313078_v_v8_aa.jpg',
+    link: 'https://movieflix9.netlify.app/magic',
+    categories: ['latestMovies']
+  },
+  {
+    title: 'The Bhootni',
+    image: 'https://i.postimg.cc/mDxKxYRF/images-4.jpg',
+    link: 'https://movieflix9.netlify.app/bhootni',
+    categories: ['latestMovies']
+  },
+  {
+    title: 'Jongli',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQgqAc6VFQWRzapb9n60cSTH7w_VNovCjgufyFY1ll6x-iaWJwB1xnv9Im&s=10',
+    link: 'https://movieflix9.netlify.app/jongli',
+    categories: ['latestMovies', 'banglaMovies']
+  },
+  {
+    title: 'Jaat (2025)',
+    image: 'https://m.media-amazon.com/images/M/MV5BNDQxYmM2YmUtOGJmYy00YzcyLTkxOTItYmI3ZTZjZmUwZDMxXkEyXkFqcGc@._V1_FMjpg_UY2048_.jpg',
+    link: 'https://movieflix9.netlify.app/jaat',
+    categories: ['latestMovies']
+  },
+  {
+    title: 'Movie 6',
+    image: 'https://m.media-amazon.com/images/M/MV5BNDY1ZWQyNjMtYTJjMC00ZjM3LTk3MWItMTcwNjg4ODZmODZhXkEyXkFqcGc@._V1_QL75_UX704_.jpg',
+    link: 'https://example.com/movie6',
+    categories: ['latestMovies']
+  },
+  {
+    title: 'Movie 7',
+    image: 'https://m.media-amazon.com/images/M/MV5BNDY1ZWQyNjMtYTJjMC00ZjM3LTk3MWItMTcwNjg4ODZmODZhXkEyXkFqcGc@._V1_QL75_UX704_.jpg',
+    link: 'https://example.com/movie7',
+    categories: ['latestMovies']
+  },
+  {
+    title: 'Movie 8',
+    image: 'movie8.jpg',
+    link: 'https://example.com/movie8',
+    categories: ['latestMovies']
+  },
+  {
+    title: 'Movie 9',
+    image: 'movie9.jpg',
+    link: 'https://example.com/movie9',
+    categories: ['latestMovies', 'tvwebseries']
+  },
+  {
+    title: 'Movie 10',
+    image: 'movie10.jpg',
+    link: 'https://example.com/movie10',
+    categories: ['latestMovies', 'tvwebseries']
+  },
+  {
+    title: 'Movie 11',
+    image: 'movie11.jpg',
+    link: 'https://example.com/movie11',
+    categories: ['latestMovies', 'tvwebseries']
+  },
+  {
+    title: 'Movie 12',
+    image: 'movie12.jpg',
+    link: 'https://example.com/movie12',
+    categories: ['latestMovies', 'tvwebseries']
+  },
+  {
+    title: 'Movie 13',
+    image: 'movie13.jpg',
+    link: 'https://example.com/movie13',
+    categories: ['latestMovies', 'tvwebseries']
+  },
+  {
+    title: 'Movie 14',
+    image: 'movie14.jpg',
+    link: 'https://example.com/movie14',
+    categories: ['latestMovies', 'tvwebseries']
+  },
+  {
+    title: 'Movie 15',
+    image: 'movie15.jpg',
+    link: 'https://example.com/movie15',
+    categories: ['latestMovies']
+  },
+  {
+    title: 'Hindi Movie 1',
+    image: 'hindi1.jpg',
+    link: 'https://example.com/hindi1',
+    categories: ['hindiDubbedMovies']
+  },
+  {
+    title: 'Hollywood Movie 1',
+    image: 'hollywood1.jpg',
+    link: 'https://example.com/hollywood1',
+    categories: ['hollywoodMovies']
+  },
+  {
+    title: 'Bangla Dubbed Movie 1',
+    image: 'bangladubbed1.jpg',
+    link: 'https://example.com/bangladubbed1',
+    categories: ['banglaDubbedMovies']
+  },
+  {
+    title: 'Emoji',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyBGj3VcgaQ2J9I3tl68_HC84kNulomfDthLV2FcZKK_RfO_mj0VOiAGw&s=10',
+    link: 'https://movieflix9.netlify.app/emoji',
+    categories: ['tvwebseries']
+  },
+  {
+    title: 'Bhog (2025)',
+    image: 'https://m.media-amazon.com/images/M/MV5BZjllZjRiZGEtNzc2Mi00ZmFmLTllYWEtYzYxMThhMTgxYjM3XkEyXkFqcGc@._V1_QL75_UX808_.jpg',
+    link: 'https://movieflix9.netlify.app/bhog',
+    categories: ['tvwebseries']
+  },
+  {
+    title: 'Anime Movie 1',
+    image: 'anime1.jpg',
+    link: 'https://example.com/anime1',
+    categories: ['animeMovies']
+  }
+];
