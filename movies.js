d82c7b8a52da4823138bd5a9db3463ce6e7c79be
selimgazi999 <selimonline51@gmail.com>
2025-05-12 22:48:16 +0600

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,14 +1,51 @@
 const movies = [
   {
-    title: "Magic (2021)",
+    title: "Borbaad (2025)",
+    image: "https://m.media-amazon.com/images/M/MV5BNDY1ZWQyNjMtYTJjMC00ZjM3LTk3MWItMTcwNjg4ODZmODZhXkEyXkFqcGc@._V1_QL75_UX704_.jpg",
+    link: "https://movieflix9.netlify.app/borbaadhd",
+    categories: ["latestMovies", "banglaMovies"]
+  },
+  {
+    title: "Daagi (2025)",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRNMkb9QpwE4K-czQ1nWEdwsORy-_zj5NhkiIZfVRurr9dje_m5Ob0VZaT&s=10",
+    link: "https://movieflix9.netlify.app/daagi",
+    categories: ["latestMovies", "banglaMovies"],
+    comingSoon: true
+  },
+  {
+    title: "The Bhootni (2025)",
+    image: "https://i.postimg.cc/MGm9VvFm/20250512-002743.png",
+    link: "https://movieflix9.netlify.app/bhootni",
+    categories: ["latestMovies", "hindiDubbedMovies"]
+  },
+  {
+    title: "Magic Movie",
     image: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p26313078_v_v8_aa.jpg",
     link: "https://movieflix9.netlify.app/magic",
     categories: ["latestMovies", "banglaMovies"]
   },
   {
-    title: "Borbaad (2025)",
-    image: "https://m.media-amazon.com/images/M/MV5BNDY1ZWQyNjMtYTJjMC00ZjM3LTk3MWItMTcwNjg4ODZmODZhXkEyXkFqcGc@._V1_QL75_UX704_.jpg",
-    link: "https://movieflix9.netlify.app/borbaadhd",
+    title: "Jongli (2025)",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQgqAc6VFQWRzapb9n60cSTH7w_VNovCjgufyFY1ll6x-iaWJwB1xnv9Im&s=10",
+    link: "https://movieflix9.netlify.app/jongli",
     categories: ["latestMovies", "banglaMovies"]
+  },
+  {
+    title: "Jaat (2025",
+    image: "https://m.media-amazon.com/images/M/MV5BNDQxYmM2YmUtOGJmYy00YzcyLTkxOTItYmI3ZTZjZmUwZDMxXkEyXkFqcGc@._V1_FMjpg_UY2048_.jpg",
+    link: "https://movieflix9.netlify.app/jaat",
+    categories: ["latestMovies", "hindiDubbedMovies"]
+  },
+  {
+    title: "Bhog (2025)",
+    image: "https://m.media-amazon.com/images/M/MV5BZjllZjRiZGEtNzc2Mi00ZmFmLTllYWEtYzYxMThhMTgxYjM3XkEyXkFqcGc@._V1_QL75_UX808_.jpg",
+    link: "https://movieflix9.netlify.app/bhog",
+    categories: ["tvwebseries"]
+  },
+  {
+    title: "Emoji(2025)",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyBGj3VcgaQ2J9I3tl68_HC84kNulomfDthLV2FcZKK_RfO_mj0VOiAGw&s=10",
+    link: "https://movieflix9.netlify.app/emoji",
+    categories: ["banglaDubbedMovies", "tvwebseries"]
   }
 ];
