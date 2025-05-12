ace15b44b8f5d6b03fe0f9695397a0ebc5f0ac6e
EMONGAZI19 <afranemon84@gmail.com>
2025-05-12 13:15:20 +0600

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,15 @@
+const movies = [
+  {
+    title: "Magic (2021)",
+    image: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p26313078_v_v8_aa.jpg",
+    link: "https://movieflix9.netlify.app/magic",
+    categories: ["latestMovies", "banglaMovies"]
+  },
+  {
+    title: "Borbaad (2025)",
+    image: "https://m.media-amazon.com/images/M/MV5BNDY1ZWQyNjMtYTJjMC00ZjM3LTk3MWItMTcwNjg4ODZmODZhXkEyXkFqcGc@._V1_QL75_UX704_.jpg",
+    link: "https://movieflix9.netlify.app/borbaadhd",
+    categories: ["latestMovies", "banglaMovies"],
+    comingSoon: true
+  }
+];
