087d8d7fdc9be4ebd3ec65d255c5ed4d3cbac21b
EMONGAZI19 <afranemon84@gmail.com>
2025-05-12 13:17:36 +0600

    Update movies.js

--- movies.js
+++ movies.js
@@ -9,7 +9,6 @@ const movies = [
     title: "Borbaad (2025)",
     image: "https://m.media-amazon.com/images/M/MV5BNDY1ZWQyNjMtYTJjMC00ZjM3LTk3MWItMTcwNjg4ODZmODZhXkEyXkFqcGc@._V1_QL75_UX704_.jpg",
     link: "https://movieflix9.netlify.app/borbaadhd",
-    categories: ["latestMovies", "banglaMovies"],
-    comingSoon: true
+    categories: ["latestMovies", "banglaMovies"]
   }
 ];
